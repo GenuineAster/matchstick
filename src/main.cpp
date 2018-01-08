beffff3ec85ac79c7628e4959332feb6896aa006
Mischa Aster Alff <aster@is.a.cxx.ninja>
2018-01-08 14:36:31 +0100

    make markov property deeper (2 instead of 1)

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <random>
 #include <unordered_map>
 
+using StringType = std::string;
+
 struct ApplicationArgs {
 	std::optional<std::string> fname;
 };
@@ -27,17 +29,15 @@ ApplicationArgs parse_args(int argc, char **argv) {
 	return result;
 }
 
-std::string read_file(const std::string &filename) {
-	if (std::ifstream f(filename); f.is_open()) {
+StringType read_file(const std::string &filename) {
+	if (std::ifstream f(filename, std::ios::binary); f.is_open()) {
 		return {std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>()};
 	}
-	return "";
+	return {};
 };
 
-
-
-std::vector<std::string> tokenize(const std::string &input) {
-	std::vector<std::string> tokens;
+std::vector<StringType> tokenize(const StringType &input) {
+	std::vector<StringType> tokens;
 	std::size_t token_start = 0;
 	for (int i = 0; i < input.length(); ++i) {
 		bool get_word = false;
@@ -85,6 +85,11 @@ std::vector<std::string> tokenize(const std::string &input) {
 			get_punctuation = true;
 			get_word = true;
 		}
+		else if (input[i] == '+') {
+			get_punctuation = true;
+			get_word = true;
+		}
+
 		if (get_word) {
 			if (i-token_start >= 1) {
 				tokens.push_back(input.substr(token_start, i-token_start));
@@ -95,17 +100,17 @@ std::vector<std::string> tokenize(const std::string &input) {
 			token_start = i+1;
 		}
 		if (get_punctuation) {
-			tokens.push_back(std::string(input.substr(i, 1)));
+			tokens.push_back(StringType(input.substr(i, 1)));
 		}
 	}
 	return tokens;
 }
 
-std::vector<std::uint64_t> hash_tokens(const std::vector<std::string> &tokens) {
+std::vector<std::uint64_t> hash_tokens(const std::vector<StringType> &tokens) {
 	std::vector<std::uint64_t> result;
 	result.reserve(tokens.size());
 	for (int i = 0; i < tokens.size(); ++i) {
-		result.push_back(std::hash<std::string>()(tokens[i]));
+		result.push_back(std::hash<StringType>()(tokens[i]));
 	}
 	return result;
 }
@@ -143,54 +148,89 @@ int main(int argc, char **argv) {
 		id_translation.push_back(hash_to_id[hash]);
 	}
 
-	std::unordered_map<std::uint64_t, std::vector<std::uint64_t>> graph;
+	std::unordered_map<std::uint64_t, std::unordered_map<std::uint64_t, std::vector<std::uint64_t>>> graph;
 
-	for (int i = 0; i < id_translation.size()-1; ++i) {
-		graph[id_translation[i]].push_back(id_translation[i+1]);
+	for (int i = 1; i < id_translation.size()-1; ++i) {
+		graph[id_translation[i-1]][id_translation[i]].push_back(id_translation[i+1]);
 	}
 
-	std::vector<std::uint64_t> output;
-	std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_int_distribution<std::uint64_t> dist(0, unique_hashed_tokens.size()-1);
-	uint64_t first_word = 0;
-	do {
-		first_word = hash_to_id[unique_hashed_tokens[dist(mt)]];
-	} while ( ! std::isalpha(unique_tokens[first_word][0]));
+	for (int iteration = 0; iteration < 20; ++iteration) {
+		std::vector<std::uint64_t> output;
+		std::random_device rd;
+		std::mt19937 mt(rd());
+		std::uniform_int_distribution<std::uint64_t> dist(0, unique_hashed_tokens.size()-1);
+		std::uniform_int_distribution<std::uint64_t> complexity_dist(0, 45);
 
-	output.push_back(first_word);
+		uint64_t strain_length = 0;
+		for (int i = 0; i < 100; ++i) {
+			const uint64_t random_graph_complexity = 99;// complexity_dist(mt);
+			if (output.size() > 3 && output[i-3] == output[i-2] && output[i-2] == output[i-1]) {
+				break;
+			}
 
-	for (int i = 1; i < 100; ++i) {
-		const auto &next_vec = graph[output.back()];
-		if (next_vec.empty())
-			break;
+			if (random_graph_complexity == 0 || strain_length == 0) {
+				uint64_t next_word_id = 0;
+				do {
+					next_word_id = hash_to_id[unique_hashed_tokens[dist(mt)]];
+				} while ( ! std::isalpha(unique_tokens[next_word_id][0]));
 
-		std::uniform_int_distribution<std::uint64_t> next_dist(0, next_vec.size()-1);
-		output.push_back(next_vec[next_dist(mt)]);
-	}
+				output.push_back(next_word_id);
+				strain_length = 1;
+			} else if (output.size() == 1 || random_graph_complexity == 1 || strain_length == 1) {
+				const auto &next_vec = graph[output[i-1]];
+				if (next_vec.empty())
+					break;
 
-	//bool insert_space = false;
-	bool capitalize_next_word = true;
-	for (const auto &word : output) {
-		if (unique_tokens[word] == "." || unique_tokens[word] == "?" || unique_tokens[word] == "!" || unique_tokens[word] == ","
-			|| unique_tokens[word] == ";" || unique_tokens[word] == ":" || unique_tokens[word] == "\"") {
-			//insert_space = true;
-		} else {
-			std::cout << " ";
+				std::uniform_int_distribution<std::uint64_t> next_dist(0, next_vec.size()-1);
+				auto it = next_vec.begin();
+				const auto dist = next_dist(mt);
+				std::advance(it, dist);
+				output.push_back(it->first);
+				strain_length = 2;
+			} else {
+				const auto &next_vec = graph[output[i-2]][output[i-1]];
+				if (next_vec.empty())
+					break;
+
+				std::uniform_int_distribution<std::uint64_t> next_dist(0, next_vec.size()-1);
+				auto it = next_vec.begin();
+				const auto dist = next_dist(mt);
+				std::advance(it, dist);
+				output.push_back(*it);
+				strain_length = 3;
+			}
+
+			if (unique_tokens[output.back()] == "\n")
+				break;
 		}
 
-		if (capitalize_next_word) {
-			std::cout << (char)std::toupper(unique_tokens[word][0]) << unique_tokens[word].substr(1);
-		}
-		else {
-			std::cout << unique_tokens[word];
-		}
+		if (output.size() < 4)
+			continue;
 
-		if (unique_tokens[word] == "." || unique_tokens[word] == "?" || unique_tokens[word] == "!") {
-			//insert_space = true;
-			capitalize_next_word = true;
-		} else {
-			capitalize_next_word = false;
+		bool capitalize_next_word = true;
+		bool first_word = true;
+		for (const auto &word : output) {
+			if (first_word != true && unique_tokens[word] != "." && unique_tokens[word] != "?" && unique_tokens[word] != "!" && unique_tokens[word] != "," &&
+				unique_tokens[word] != ";" && unique_tokens[word] != ":" && unique_tokens[word] != "\"") {
+				std::cout << " ";
+			}
+
+			if (first_word)
+				first_word = false;
+
+			if (capitalize_next_word) {
+				std::cout << (char)std::toupper(unique_tokens[word][0]) << unique_tokens[word].substr(1);
+			}
+			else {
+				std::cout << unique_tokens[word];
+			}
+			
+
+			if (unique_tokens[word] == "." || unique_tokens[word] == "?" || unique_tokens[word] == "!") {
+				capitalize_next_word = true;
+			} else {
+				capitalize_next_word = false;
+			}
 		}
 	}
 
